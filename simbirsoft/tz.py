a65811a36e9a74728d34352deb6fd739f8950de9
dkwlacke <maxsan2006@gmail.com>
2025-03-07 21:14:12 +0300

    Add files via upload

--- simbirsoft/tz.py
+++ simbirsoft/tz.py
@@ -4,24 +4,23 @@ from selenium.webdriver.support.ui import Select
 import time
 import unittest
 
-class TestAbs(unittest.TestCase):
+class Test(unittest.TestCase):
    def test(self):
     link = "https://practice-automation.com/form-fields/"
     browser = webdriver.Chrome()
-    #browser.implicitly_wait(5)
     browser.get(link)
     select = Select(browser.find_element(By.TAG_NAME, "select"))
     input1=browser.find_element(By.ID, "name-input")
-    input1.send_keys("qewqeqwe")
+    input1.send_keys("Maksim")
     input2 = browser.find_element(By.XPATH, "//input[@type='password']")
-    input2.send_keys("qwert")
+    input2.send_keys("qwerty")
     milk=browser.find_element(By.ID, "drink2")
     milk.click()
     time.sleep(5)
     coffe=browser.find_element(By.ID, "drink3")
     coffe.click()
     email=browser.find_element(By.ID, "email")
-    email.send_keys("qwertyu")
+    email.send_keys("qwerty@gmail.com")
     select.select_by_visible_text("Yes")
     msg=browser.find_element(By.ID, "message")
     tool=browser.find_elements(By.XPATH, "//form/ul/li")
